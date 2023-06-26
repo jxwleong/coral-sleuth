b57145ddec7fbc0999363fe830f548f05802b061
jxwleong <jason.xie.wei.leong@gmail.com>
2023-06-26 11:13:19 +0800

    Remove powershell comments as I already created a seperate powershell script

--- combine_dataset.py
+++ combine_dataset.py
@@ -28,13 +28,3 @@ for filename in os.listdir(dir_path):
                     # append to csv
                     writer.writerow([image_name] + row_col_label)
 
-
-# Using PS to copy the .jpg files
-""" 
-$source = 'C:\Users\ad_xleong\Desktop\coral-sleuth\images\MCR_LTER_ComputerVision_LabeledCorals_2008_2009_2010\2010'
-$destination = 'C:\Users\ad_xleong\Desktop\coral-sleuth\images\'
-Get-ChildItem -Path $source -Filter *.jpg | ForEach-Object {
-    Copy-Item -Path $_.FullName -Destination $destination
-}
-
-"""
