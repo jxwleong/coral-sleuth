a2c6f4fc1f3d165746c96375b7b937c562af25d6
jxwleong <jason.xie.wei.leong@gmail.com>
2023-06-28 09:59:53 +0800

    Added script to quickly show the label distribution of the annotation file

--- src/scripts/label_distribution_analysis.py
+++ src/scripts/label_distribution_analysis.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
+# Show all rows instead of "..." in the middle
+pd.set_option('display.max_rows', None)
 
 # Load your CSV file
 df = pd.read_csv(r"C:\Users\ad_xleong\Desktop\coral-sleuth\data\annotations\combined_annotations.csv")
@@ -14,7 +18,7 @@ print("Label distribution:")
 print(label_counts)
 
 # You could also visualize this distribution
-#label_counts.plot(kind='bar')
+label_counts.plot(kind='bar')
 
-
+plt.show()  # This will show the plot
 
