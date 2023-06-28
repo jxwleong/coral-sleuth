21a625295f423b1a7d6e5322762834493516cf72
jxwleong <jason.xie.wei.leong@gmail.com>
2023-06-28 13:57:15 +0800

    Update count_image.ps1 to count jpg files as well

--- src/scripts/count_image.ps1
+++ src/scripts/count_image.ps1
@@ -1,3 +1,3 @@
 $source = 'C:\Users\ad_xleong\Desktop\coral-sleuth\data\images'
 
-(Get-ChildItem -Path $source -Include *.png -Recurse).Count
+(Get-ChildItem -Path $source -Include *.png,*.jpg -Recurse).Count
