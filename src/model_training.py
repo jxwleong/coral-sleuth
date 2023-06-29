e27b34ae51c95026794290664bd626062f324ebf
jxwleong <jason.xie.wei.leong@gmail.com>
2023-06-29 16:14:04 +0800

    Update the naming of the metrics_file to include epochs and batch size

--- src/model_training.py
+++ src/model_training.py
@@ -18,15 +18,15 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    #annotation_file = os.path.join(ANNOTATION_DIR, "combined_annotations_about_40k.csv")
+    #annotation_file = os.path.join(ANNOTATION_DIR, "combined_annotations_about_40k_png_only.csv")
     annotation_file = os.path.join(ANNOTATION_DIR, "combined_annotations_1000.csv")
 
     batch_size = 16
-    epoch = 1
+    epoch = 50
    
     # for each classifier
-    for model_type in ['efficientnet', 'vgg16', 'mobilenetv3', 'custom']:
-    #for model_type in ['mobilenetv3']:
+    for model_type in ['efficientnet', 'efficientnetb0','vgg16', 'mobilenetv3', 'custom']:
+    #for model_type in ['efficientnetb0']:
         classifier = CoralReefClassifier(ROOT_DIR, DATA_DIR, IMAGE_DIR, annotation_file, model_type)
         classifier.create_model()
         logger.info(f"Start model({model_type}) training...")
@@ -48,7 +48,7 @@ if __name__ == "__main__":
         metrics["annotation_file"] = classifier.annotation_file 
 
         # Save metrics to a JSON file
-        metrics_file = os.path.join(MODEL_DIR,f'coral_reef_classifier_{model_type}_metrics.json')
+        metrics_file = os.path.join(MODEL_DIR,f'coral_reef_classifier_{model_type}_epoch_{epoch}_1_batchsize_{batch_size}_metrics.json')
         with open(metrics_file, 'w') as f:
             json.dump(metrics, f, indent=4)
 
