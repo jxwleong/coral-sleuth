3b1397ac9dc6eeae4101303dbcfac74a7581df99
jxwleong <jason.xie.wei.leong@gmail.com>
2023-06-29 08:21:29 +0800

    Added code to remove icc_profile during jpg to png conversion to remove the warning 'libpng warning: iCCP: known incorrect sRGB profile' during model training

--- src/scripts/convert_jpg_to_png.py
+++ src/scripts/convert_jpg_to_png.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageFile
 import os
 import sys
 
-
 ROOT_DIR = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
 sys.path.insert(0, ROOT_DIR)
 from config.path import IMAGE_DIR
@@ -13,16 +12,23 @@ output_folder = input_folder  # Saving output to the same folder
 # Tell PIL to accept truncated images
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
+# Set to True if you want to overwrite existing files
+overwrite_existing_files = True
+
 for jpeg_file in os.listdir(input_folder):
     if jpeg_file.endswith(".jpg"):
         png_file = os.path.splitext(jpeg_file)[0] + ".png"
         output_file = os.path.join(output_folder, png_file)
-        if os.path.exists(output_file) is True:
+        if os.path.exists(output_file) and not overwrite_existing_files:
             print(f"{output_file} exists! Skipping...")
             continue
         try:
             print(f"Converting {jpeg_file} to png... ", end=" ")
             img = Image.open(os.path.join(input_folder, jpeg_file))
+            # Remove ICC profile as it caused this issue:
+            # libpng warning: iCCP: known incorrect sRGB profile
+            if 'icc_profile' in img.info:
+                del img.info['icc_profile']
             img.save(output_file)
             print("DONE")
         except Exception as e:
