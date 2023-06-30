8b6501fc88207a544bce5d5d4d6b7b956c712303
jxwleong <jason.xie.wei.leong@gmail.com>
2023-06-30 13:02:00 +0800

    Update model and metric naming with annotation name

--- src/model_training.py
+++ src/model_training.py
@@ -1,3 +1,8 @@
+""" 
+Run in remote SSH
+Start-Process -NoNewWindow -FilePath "python" -ArgumentList "model_training.py" 
+"""
+
 import os
 import numpy as np
 import csv
@@ -5,6 +10,8 @@ import cv2
 import json 
 import time 
 import logging
+
+from tensorflow.python.client import device_lib
 
 import sys
 ROOT_DIR = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", ".."))
@@ -18,22 +25,28 @@ logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    #annotation_file = os.path.join(ANNOTATION_DIR, "combined_annotations_about_40k_png_only.csv")
-    annotation_file = os.path.join(ANNOTATION_DIR, "combined_annotations_1000.csv")
+    annotation_filename = "annotations_coralnet_only_trimmed.csv"
+    annotation_name = annotation_filename.split(".")[0]
+    annotation_filepath = os.path.join(ANNOTATION_DIR, annotation_filename)
 
     batch_size = 16
-    epoch = 50
-   
+    epoch = 1
+    
+    logger.info(f"Device List: {device_lib.list_local_devices()}")
+
     # for each classifier
     for model_type in ['efficientnet', 'efficientnetb0','vgg16', 'mobilenetv3', 'custom']:
     #for model_type in ['efficientnetb0']:
-        classifier = CoralReefClassifier(ROOT_DIR, DATA_DIR, IMAGE_DIR, annotation_file, model_type)
+        classifier = CoralReefClassifier(ROOT_DIR, DATA_DIR, IMAGE_DIR, annotation_filepath, model_type)
         classifier.create_model()
-        logger.info(f"Start model({model_type}) training...")
+        logger.info(f"Start model ({model_type}) training...")
         classifier.train(batch_size=batch_size, epochs=epoch)
 
-        logger.info(f"Training model({model_type}) DONE!")
-        model_file = os.path.join(MODEL_DIR, f'coral_reef_classifier_{model_type}_full_epoch_{epoch}_1_batchsize_{batch_size}.h5')
+        logger.info(f"Training model ({model_type}) DONE!")
+        model_file = os.path.join(
+            MODEL_DIR, 
+            f'coral_reef_classifier_{model_type}_full_epoch_{epoch}_1_batchsize_{batch_size}_{annotation_name}.h5'
+        )
         classifier.save_model(model_file)
 
         logger.info(f"{model_file} SAVED!")
@@ -45,11 +58,15 @@ if __name__ == "__main__":
         # Make sure already run model.train to get this attribute
         metrics["traning_time_in_seconds"] = classifier.training_time
 
-        metrics["annotation_file"] = classifier.annotation_file 
+        metrics["annotation_filepath"] = classifier.annotation_filepath 
 
         # Save metrics to a JSON file
-        metrics_file = os.path.join(MODEL_DIR,f'coral_reef_classifier_{model_type}_epoch_{epoch}_1_batchsize_{batch_size}_metrics.json')
+        metrics_file = os.path.join(
+            MODEL_DIR,
+            f'coral_reef_classifier_{model_type}_epoch_{epoch}_1_batchsize_{batch_size}_metrics_{annotation_name}.json'
+        )
         with open(metrics_file, 'w') as f:
             json.dump(metrics, f, indent=4)
 
         logger.info(f"Evaluation metrics saved: {metrics_file}")
+
