fb557f02edcb341158ab1e72343cc61e76e7c561
jxwleong <jason.xie.wei.leong@gmail.com>
2023-07-02 16:53:25 +0800

    Added seperate additional labels to remove list and use it to add additional labels

--- src/scripts/remove_labels_from_annotation_file.py
+++ src/scripts/remove_labels_from_annotation_file.py
@@ -7,15 +7,15 @@ sys.path.insert(0, ROOT_DIR)
 
 from config.path import ANNOTATION_DIR
 
-input_annotation =  os.path.join(ANNOTATION_DIR, "annotations_coralnet_only.csv")
-output_annotation = os.path.join(ANNOTATION_DIR, "annotations_coralnet_only_trimmed.csv")
+input_annotation =  os.path.join(ANNOTATION_DIR, "combined_annotations_about_40k_png_only.csv")
+output_annotation = os.path.join(ANNOTATION_DIR, "combined_annotations_about_40k_png_CCA.csv")
 
 # Load CSV file
 df = pd.read_csv(input_annotation)
 
 # Define the labels want to remove
 labels_to_remove = [
-    'Sand',
+'Sand',
     'Acropora',
     'Acr_dig',
     'Pavona',
@@ -38,9 +38,52 @@ labels_to_remove = [
     'GFA',        
     'Favites',        
     'Astreo',          
-    'fav'
+    'fav',
+    'Mille', 
+    'Lepta', 
+    'Millepora', 
+    'Monta', 
+    'MILLE', 
+    'ACROP', 
+    'P mass', 
+    'Soft', 
+    'Fung', 
+    'MONTA', 
+    'Gardin', 
+    'LEPTA', 
+    'SOFT', 
+    'Herpo', 
+    'FUNG', 
+    'Lobo', 
+    'P. Irr', 
+    'GARDIN', 
+    'ASTREO', 
+    'Favia'
 ]
 
+additional_labels_to_remove = [
+    'Turf',
+    'Porit',
+    'P. Rus',
+    'TURF',
+    'HS',
+    'Macro',
+    'SAND',
+    'PORIT',
+    'MACRO',
+    'C-Rubble',
+    'Off',
+    'OFF',
+    'Pavon',
+    'POCILL',
+    'MONTI',
+    'PAVON',
+    'Dark',
+    'Algae',
+    'Acrop'
+]
+
+labels_to_remove.extend(additional_labels_to_remove)
 
 # Remove rows where Label is in `labels_to_remove`
 df = df[~df['Label'].isin(labels_to_remove)]
