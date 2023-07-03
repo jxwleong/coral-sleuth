16c881c94174df95e4ebab15f5f35dfff6786648
jxwleong <jason.xie.wei.leong@gmail.com>
2023-07-03 14:12:20 +0800

    Added annotation label mapping to remap similar labels

---
+++ config/annotation.py
@@ -0,0 +1,126 @@
+# Mapping used to normalize the similar labels in the annotation files
+# U can find most of the labels information here: https://coralnet.ucsd.edu/label/list/
+
+label_mapping = {
+    "Sand": "sand",
+    "SAND": "sand",
+    
+    "Turf": "turf",
+    "TURF": "turf",
+    
+    "Porit": "porites",
+    "PORIT": "porites",
+    
+    "Macro": "macroalgae",
+    "MACRO": "macroalgae",
+    
+    # https://coralnet.ucsd.edu/label/85/
+    "Off": "off",
+    "OFF": "off",
+    
+    "Pocill": "pocillopora",
+    "POCILL": "pocillopora",
+    
+    "Monti": "montipora",
+    "MONTI": "montipora",
+    # https://coralnet.ucsd.edu/label/621/
+    "Monti_encr": "montipora",
+    
+    "Monta": "montastraea",
+    "MONTA": "montastraea",
+    
+    "Pavon": "pavona",
+    "Pavona": "pavona",
+    "PAVON": "pavona",
+    
+    "Acrop": "acropora",
+    "ACROP": "acropora",
+    "Acropora": "acropora",
+    "Acr_dig": "acropora",
+    "Acr_tab": "acropora",
+    
+    "Mille": "millepora",
+    "MILLE": "millepora",
+    "Millepora": "millepora",
+    
+    "Lepta": "leptastrea",
+    "LEPTA": "leptastrea",
+    
+    "Lepto": "leptoseris",
+    
+    "Soft": "soft",
+    "SOFT": "soft",
+    
+    "Fung": "fungia",
+    "FUNG": "fungia",
+    
+    "Gardin": "gardineroseris",
+    "GARDIN": "gardineroseris",
+    
+    "Astreo": "astreopora",
+    "ASTREO": "astreopora",
+    
+    "Favia": "favia",
+    "FAVIA": "favia",
+    "fav": "favia",
+    
+    "Lobo": "lobophyllia",
+    "LOBO": "lobophyllia",
+    
+    "Psam": "psammocora",
+    "PSAM": "psammocora",
+    
+    "Cypha": "cyphastrea",
+    "CYPHA": "cyphastrea",
+    
+    "Acan": "acanthastrea",
+    "ACAN": "acanthastrea",
+    
+    "GFA": "green_fleshy_algae",
+    
+    "HS": "hard_substrate",
+    
+    # P. Rus
+    # https://coralnet.ucsd.edu/label/88/
+    "P. Rus": "porites",
+    # P. Irr
+    # https://coralnet.ucsd.edu/label/90/
+    "P. Irr": "porites",
+    # P mass - Most likely "Porites massiva"
+    "P mass": "porites",
+    "Porites": "porites",
+    "porites": "porites",
+    
+    "D_coral": "dead_coral",
+    
+    "Herpo": "herpolitha",
+    
+    "SC": "soft_coral",
+    
+    "Platy": "platygyra",
+    
+    "Echinopora": "echinopora", 
+    
+    "Rock": "rock",
+    
+    "Stylo": "stylophora",
+    
+    "Favites": "favites",
+    
+    "Sando": "sandolitha",
+    
+    "Tuba": "tuba",
+    
+    "Dark": "dark",
+    
+    # https://coralnet.ucsd.edu/label/1636/
+    "BAD": "bad",
+    
+    "Goniastrea": "goniastrea",
+    
+    "C-Rubble": "broken_coral_rubble",
+    
+    "CCA": "crustose_coralline_algae",
+    
+    "Algae": "algae",
+    }
