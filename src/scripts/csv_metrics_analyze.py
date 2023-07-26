9978ebe0854f56355eef895617aca2edb19a44be
jxwleong <jason.xie.wei.leong@gmail.com>
2023-07-26 21:02:22 +0800

    Added max data for analyzing the csv metrics

--- src/scripts/csv_metrics_analyze.py
+++ src/scripts/csv_metrics_analyze.py
@@ -10,12 +10,17 @@ sys.path.insert(0, ROOT_DIR)
 
 from config.path import MODEL_DIR
 
-filename = "coral_reef_classifier_efficientnetv2_epoch_200_batchsize_32_metrics_combined_annotations_about_40k_png_only_remapped_majority_class_with_3k_to_4k_sample_reduced_1k_scale_1.0.csv"
+filename = "coral_reef_classifier_efficientnetv2_epoch_200_batchsize_32_metrics_combined_annotations_about_40k_png_only_remapped_majority_class_with_3k_to_4k_sample_reduced_1k_scale_0.2.csv"
 input_file = os.path.join(MODEL_DIR, filename)
 df = pd.read_csv(input_file)
 
+print("MIN")
 # Calculate the mean of each column
 means = df.mean()
 
 # Print the mean of each column
 print(means)
+
+print("\nMAX")
+maxs = df.max()
+print(maxs)
