21ab6348849bc14248b0b5373aea33e2dd54f85b
jxwleong <jason.xie.wei.leong@gmail.com>
2023-07-26 20:16:41 +0800

    Update label_undersample.py now supported using threshold to detect which classes to undersample

--- src/scripts/label_undersample.py
+++ src/scripts/label_undersample.py
@@ -9,24 +9,18 @@ ROOT_DIR = os.path.normpath(os.path.join(os.path.abspath(__file__), "..", "..", 
 sys.path.insert(0, ROOT_DIR)
 from config.path import ANNOTATION_DIR
 
-
 filename = "combined_annotations_1000_remapped.csv"
 filepath = os.path.join(ANNOTATION_DIR, filename)
 
 output_filename = filename.replace(".csv", "") + "_undersample.csv"
 outfile = os.path.join(ANNOTATION_DIR, output_filename)
 
-labels_to_undersample = [
-    "porites",
-    "crustose_coralline_algae",
-    "turf"
-]
-
-undersample_count = 10  # desired count for undersampling
+threshold = 300  # threshold for class count
+undersample_count = 100  # desired count for undersampling
 
 data = pd.read_csv(filepath)
 
-#  Count the number of instances for each class
+# Count the number of instances for each class
 class_counts = data['Label'].value_counts().sort_index()
 
 # Get the labels
@@ -38,19 +32,17 @@ initial_distribution = class_counts.tolist()
 # Perform undersampling
 undersampled_data = pd.DataFrame()
 
-for label in class_counts.index:
+for label, count in class_counts.iteritems():
     label_data = data[data['Label'] == label]
     
-    # If this is one of the classes to be undersampled, randomly select instances equal to the specified count
-    if label in labels_to_undersample:
-        if len(label_data) > undersample_count:  # check if the class count is larger than the undersample count
-            label_data = label_data.sample(undersample_count)
+    # If this class count exceeds the threshold, randomly select instances equal to the undersample count
+    if count >= threshold:
+        label_data = label_data.sample(min(undersample_count, len(label_data)), random_state=123)
         
     undersampled_data = pd.concat([undersampled_data, label_data])
 
 undersampled_data = undersampled_data.sample(frac=1).reset_index(drop=True)  # shuffle the data
 undersampled_data.to_csv(outfile, index=False)
-
 
 # Get the new class distribution
 new_distribution = undersampled_data['Label'].value_counts().sort_index().tolist()
